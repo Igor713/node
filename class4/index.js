cf3dd3f082975b44900435276fe6c3aa4e8e05c2
Igor713 <igormatheussiquieri@gmail.com>
2023-10-23 22:21:12 -0300

    finished class 4 about node fs

--- class4/index.js
+++ class4/index.js
@@ -1,7 +1,33 @@
-const fs = require('fs')
-const path = require('path')
+const fs = require('fs').promises;  // Use fs.promises para acessar as versões assíncronas das funções do módulo fs
+const path = require('path');
 
 async function readdir(rootDir) {
-  rootDir = rootDir || path.resolve(__dirname)
-  const files = await fs.readdir()
+  rootDir = rootDir || path.resolve(__dirname);
+  try {
+    const files = await fs.readdir(rootDir);
+    await walk(files, rootDir);
+  } catch (error) {
+    console.error('Error reading directory:', error);
+  }
 }
+
+async function walk(files, rootDir) {
+  for (let file of files) {
+    const fileFullPath = path.resolve(rootDir, file);
+    try {
+      const stats = await fs.stat(fileFullPath);
+
+      if (stats.isDirectory()) {
+        await readdir(fileFullPath);
+        continue;
+      }
+
+      console.log(file, stats.isDirectory());
+    } catch (error) {
+      console.error('Error processing file:', file, error);
+    }
+  }
+}
+
+// Chame a função readdir
+readdir('class4').catch(error => console.error(error));
